7b37f5c8b3981dafda49d68d8c0aed952bcbafcd
Aleksandr505 <35270997+Aleksandr505@users.noreply.github.com>
2021-05-27 22:20:18 +0600

    Add files via upload

---
+++ lab6.cpp
@@ -0,0 +1,48 @@
+#include <cstring>
+#include <cstdio>
+#include<iostream>
+using namespace std;
+
+int Count = 0;
+
+struct Node {
+	Node* l;
+	Node* r;
+	int n;
+};
+
+
+Node *add_node(int N)
+{
+	if (N == 0) return NULL;
+
+	Node* p;
+	p = new Node;
+	p->n = ++Count;
+	N--;
+
+	if (N == 0) {
+		p->l = NULL;
+		p->r = NULL;
+	}
+
+	Node* left = p->l = new Node;
+	Node* right = p->r = new Node;
+	left->r = right;
+	right->l = left;
+	left->n = ++Count;
+	right->n = ++Count;
+
+	left->l = right->r = add_node(N - 1);
+
+	return p;
+}
+
+
+int main() {
+	Node* tree = add_node(6);
+
+	cout << tree->r->r->l->n << endl;
+
+	return 0;
+}
