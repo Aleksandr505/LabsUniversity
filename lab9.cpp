b11e1e902aca34ea3642318d5ff0e2238054d6f2
Aleksandr505 <35270997+Aleksandr505@users.noreply.github.com>
2021-05-28 22:42:49 +0600

    Add files via upload

---
+++ lab9.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include<iostream>
+using namespace std;
+
+int Count = 0;
+
+class Node {
+public:
+	Node* l;
+	Node* r;
+	int n;
+
+	Node() {
+		cout << "Node number " << Count << " was created" << endl;
+	}
+	~Node() {
+		cout << "Node number " << Count << " was deleated" << endl;
+	}
+};
+
+
+Node *add_node(int N)
+{
+	if (N == 0) return NULL;
+
+	Node* p;
+	p = new Node;
+	p->n = ++Count;
+	N--;
+
+	if (N == 0) {
+		p->l = NULL;
+		p->r = NULL;
+	}
+
+	Node* left = p->l = new Node;
+	Node* right = p->r = new Node;
+	left->r = right;
+	right->l = left;
+	left->n = ++Count;
+	right->n = ++Count;
+
+	left->l = right->r = add_node(N - 1);
+
+	return p;
+}
+
+
+int main() {
+	Node* tree = add_node(6);
+
+	cout << tree->l->l->r->n << endl;
+
+	return 0;
+}
